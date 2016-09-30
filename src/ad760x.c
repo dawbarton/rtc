ce530dbd01a993a95d4e05265b91a549d160895d
David AW Barton <david.barton@bristol.ac.uk>
2016-09-30 13:51:09 +0100

    Revert "Fixes to the AD760x code to ensure that data is received before entering the"
    This reverts commit b368b4f9c6d770cd0d40312f50dc2e998584893f.

--- src/ad760x.c
+++ src/ad760x.c
@@ -33,7 +33,7 @@
 ** converter.
 **
 ** Usage assumptions/notes:
-**   1) Conversion start can be either triggered manually using the
+**   1) Conversion start can be either triggered manually using the 
 **      ad760xCaptureStart function or via setting up pulse-width modulation
 **      via the DMTimer module.
 **   2) Getting the data from the device occurs automatically via interrupts.
@@ -65,7 +65,7 @@
 #include "rtc_user.h"
 
 #include "uartStdio.h"
-#include "consoleUtils.h"
+#include "consoleUtils.h"   
 
 #define AD760X_SPI_CHANNEL 0
 #define MCSPI_IN_CLK 48000000
@@ -181,7 +181,7 @@ void ad760xSetup(void)
 
     /* Perform the necessary configuration for master mode */
     /* MCSPI_DATA_LINE_COMM_MODE_7 = D0 & D1 disabled for output, receive on D1 */
-    McSPIMasterModeConfig(SOC_SPI_1_REGS, MCSPI_SINGLE_CH,
+    McSPIMasterModeConfig(SOC_SPI_1_REGS, MCSPI_SINGLE_CH, 
                           MCSPI_RX_ONLY_MODE, MCSPI_DATA_LINE_COMM_MODE_7,
                           AD760X_SPI_CHANNEL);
 
@@ -189,9 +189,9 @@ void ad760xSetup(void)
     HWREG(SOC_SPI_1_REGS + MCSPI_SYST) |= (1 << 9);
 
     /* Configure the McSPI bus clock depending on clock mode */
-    /* MCSPI_CLK_MODE_2: AD760X loads data on rising clk edge, read it on the falling
+    /* MCSPI_CLK_MODE_2: AD760X loads data on rising clk edge, read it on the falling 
        clock edge; the first bit is loaded when CS goes low */
-    McSPIClkConfig(SOC_SPI_1_REGS, MCSPI_IN_CLK, MCSPI_OUT_FREQ, AD760X_SPI_CHANNEL,
+    McSPIClkConfig(SOC_SPI_1_REGS, MCSPI_IN_CLK, MCSPI_OUT_FREQ, AD760X_SPI_CHANNEL, 
                    MCSPI_CLK_MODE_2);
 
     /* Configure the word length */
@@ -224,7 +224,7 @@ void ad760xSetup(void)
     GPIOPinMuxSetup(CONVST_PIN_MUX, PAD_FS_RXD_NA_PUPDD(7));
     GPIOPinMuxSetup(RESET_PIN_MUX,  PAD_FS_RXD_NA_PUPDD(7));
     GPIOPinMuxSetup(BUSY_PIN_MUX,   PAD_FS_RXE_PD_PUPDE(7));
-
+    
     /* Pin directions */
     GPIODirModeSet(OS0_REGS,    OS0_PIN,    GPIO_DIR_OUTPUT);
     GPIODirModeSet(OS1_REGS,    OS1_PIN,    GPIO_DIR_OUTPUT);
@@ -286,7 +286,7 @@ void ad760xSetup(void)
     /* Reset the AD760X */
     ad760xReset();
 
-    DEBUGPRINT("\t+ AD760x: device reset\r\n");
+    DEBUGPRINT("\t+ AD760x: device reset\r\n");  
 
     /* Enable the interrupt for the busy signal */
     GPIOPinIntEnable(BUSY_REGS, GPIO_INT_LINE_1, BUSY_PIN);
@@ -317,14 +317,14 @@ void ad760xSetupPWM(unsigned int period)
 void ad760xEnablePWM()
 {
     /* Enable the timer */
-    DMTimerEnable(SOC_DMTIMER_6_REGS);
+    DMTimerEnable(SOC_DMTIMER_6_REGS);  
 }
 
 /* ************************************************************************ */
 void ad760xDisablePWM()
 {
     /* Enable the timer */
-    DMTimerDisable(SOC_DMTIMER_6_REGS);
+    DMTimerDisable(SOC_DMTIMER_6_REGS);  
 }
 
 /* ************************************************************************ */
@@ -408,9 +408,6 @@ void ad760xMcSPIIsr(void)
             /* Disable interrupts */
             McSPIIntDisable(SOC_SPI_1_REGS, MCSPI_INT_TX_EMPTY(AD760X_SPI_CHANNEL) | MCSPI_INT_RX_FULL(AD760X_SPI_CHANNEL) | MCSPI_INT_EOWKE);
 
-            /* Signal data ready */
-            ad760xBufferReady = 1;
-
         }
 
         /* Check for end-of-word-count */
@@ -437,14 +434,11 @@ void ad760xGPIOIsr(void)
         /* Set the start conversion signal low again */
         GPIOPinWrite(CONVST_REGS, CONVST_PIN, GPIO_PIN_LOW);
 
-        /* Make sure that the buffer ready flag is removed */
-        ad760xBufferReady = 0;
-
-        /* Get the data (asyncronously) */
+        /* Get the data */
         ad760xCaptureGet();
 
-        /* Wait for the data */
-        while (!ad760xBufferReady);
+        /* Signal data ready */
+        ad760xBufferReady = 1;
 
         /* Call the data handler if necessary */
         if (ad760xDataHandler != NULL)
