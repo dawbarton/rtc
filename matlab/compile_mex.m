1162e5f2576e5fe46d74c93429f416b5206818cf
David AW Barton <david.barton@bristol.ac.uk>
2019-05-14 15:20:00 +0100

    Updates to libusb for Windows 10

--- matlab/compile_mex.m
+++ matlab/compile_mex.m
@@ -2,22 +2,15 @@ function compile_mex()
 % COMPILE_MEX Compile the .c files into Matlab useable MEX files.
 %
 % To compile, the development libraries for libusb are needed. For Linux
-% these should be available via your system package manager (e.g., sudo yum
+% these should be available via your system package manager (e.g., sudo dnf
 % install libusb-devel for Fedora). For Windows  see <http://libusb.info>;
-% the necessary .lib file is in the MS32 or MS64 folder of the Windows
-% binary download depending on whether you are running 32 bit or 64 bit
-% Windows. 
+% this compiles against a static libusb library. The binary used should be
+% chosen depending on whether you are running 32 bit or 64 bit Windows. 
 
 if ispc
-    % For some reason the Windows version of Matlab doesn't seem to be able
-    % to handle library file names with periods in (e.g., libusb-1.0.lib)
-    % and instead truncates them (to libusb-1.lib in this case)
-    if ~exist('libusb-1.lib', 'file') && exist('libusb-1.0.lib', 'file')
-        copyfile('libusb-1.0.lib', 'libusb-1.lib');
-    end
-    mex -largeArrayDims libusb.c -lusb-1
+    mex -largeArrayDims libusb.c libusb-1.0.a
 else
-    mex -largeArrayDims libusb.c -lusb-1.0
+    mex -largeArrayDims libusb.c -lusb-1.0 -L.
 end
 
 mex -largeArrayDims crc32.c
